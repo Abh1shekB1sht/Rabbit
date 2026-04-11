97bb3b04844aad71ff9083c0bbe47726dc5523c1
Abhishek <singhbisht84484@gmail.com>
2026-04-11 12:59:04 +0530

    feat: update UserLayout to include Outlet for nested routing

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Common/Footer';
 import Header from '../Common/Header';
 
 const UserLayout = () => {
 	return (
 		<>
+			{/* Header */}
 			<Header />
 			{/* Main Content */}
+			<main>
+				<Outlet />
+			</main>
 			{/* Footer */}
+			<Footer />
 		</>
 	);
 };
