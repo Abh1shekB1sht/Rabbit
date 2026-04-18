13d35ad91abcdbc9a77e13d4f6df34358d397742
Abhishek <singhbisht84484@gmail.com>
2026-04-18 12:55:26 +0530

    feat: configure environment variables and connect to MongoDB in server setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoute = require('./routes/userRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+dotenv.config();
+
+// Connect To MongoDB
+connectDB();
+
 app.get('/', (req, res) => {
 	res.send('Hello from the backend!');
 });
 
-app.listen(9000, () => {
+// API Routes
+app.use('/api/users', userRoute);
+
+app.listen(process.env.PORT, () => {
 	console.log('Server is running on port 9000');
 });
