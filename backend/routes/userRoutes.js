54ca53ab53c9daf16e5dac766a7fe0d44346da94
Abhishek <singhbisht84484@gmail.com>
2026-04-18 17:47:52 +0530

    feat: enhance user registration and login routes with improved validation and response handling

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,6 +9,12 @@ const router = express.Router();
 // @access Public
 router.post('/register', async (req, res) => {
 	const { name, email, password } = req.body;
+	// Basic validation
+	if (!name || !email || !password) {
+		return res.status(400).json({
+			message: 'Name, email and password are required',
+		});
+	}
 	try {
 		const existingUser = await User.findOne({ email });
 		if (existingUser) {
@@ -19,17 +25,86 @@ router.post('/register', async (req, res) => {
 		const user = new User({ name, email, password });
 		await user.save();
 
-		res.status(201).json({
-			user: {
-				_id: user._id,
-				name: user.name,
-				email: user.email,
-				role: user.role,
+		// Create JWT Payload
+		const pyaload = { user: { _id: user._id, role: user.role } };
+
+		// Sign and return the token along with user info
+		const token = jwt.sign(
+			pyaload,
+			process.env.JWT_SECRET,
+			{
+				expiresIn: '7d',
 			},
-		});
+			(err, token) => {
+				if (err) throw err;
+
+				// Send the user and token in response
+				res.status(201).json({
+					user: {
+						_id: user._id,
+						name: user.name,
+						email: user.email,
+						role: user.role,
+					},
+				});
+			},
+		);
 	} catch (error) {
-		res.status(500).json({ success: false, message: 'Server error' });
+		res.status(500).json({ message: error.message || 'Server error' });
 	}
+});
+
+// @route POST /api/users/login
+// @desc Authenticate user
+// @access Public
+router.post('/login', async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		// Find the user by email
+		let user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ message: 'Invalid Email or Password' });
+		}
+		const isMatch = await user.matchPassword(password);
+		if (!isMatch) {
+			return res.status(400).json({ message: 'Invalid Credentials' });
+		}
+
+		// Create JWT Payload
+		const pyaload = { user: { _id: user._id, role: user.role } };
+
+		// Sign and return the token along with user info
+		const token = jwt.sign(
+			pyaload,
+			process.env.JWT_SECRET,
+			{
+				expiresIn: '7d',
+			},
+			(err, token) => {
+				if (err) throw err;
+
+				// Send the user and token in response
+				res.json({
+					user: {
+						_id: user._id,
+						name: user.name,
+						email: user.email,
+						role: user.role,
+					},
+				});
+			},
+		);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: error.message || 'Server error' });
+	}
+});
+
+// @route GET /api/users/profile
+// @desc Get logged-in user's profile (Protected Route);
+// @access Private
+router.get('/profile', protect, async (req, res) => {
+	res.json(req.user);
 });
 
 module.exports = router;
