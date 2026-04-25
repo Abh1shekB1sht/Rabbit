a8394a10b924a38d5e7f14d9b93a25d69c5e5300
Abhishek <singhbisht84484@gmail.com>
2026-04-25 15:35:39 +0530

    feat: integrate product fetching with filters and enhance ProductGrid with loading and error states

--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -1,13 +1,25 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { FaFilter } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useSearchParams } from 'react-router-dom';
 import FilterSidebar from '../components/Products/FilterSidebar';
 import ProductGrid from '../components/Products/ProductGrid';
 import SortOptions from '../components/Products/SortOptions';
+import { fetchProductsByFilters } from '../redux/slices/productSlice';
 
 const CollectionPage = () => {
-	const [products, setProducts] = useState([]);
+	const { collection } = useParams();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const dispatch = useDispatch();
+	const { products, loading, error } = useSelector((state) => state.products);
+	const queryParams = Object.fromEntries([...searchParams]);
+
 	const sidebarRef = useRef(null);
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	useEffect(() => {
+		dispatch(fetchProductsByFilters({ ...queryParams, collection }));
+	}, [dispatch, collection, searchParams]);
 
 	const toggleSidebar = () => {
 		setIsSidebarOpen(!isSidebarOpen);
@@ -27,94 +39,6 @@ const CollectionPage = () => {
 		return () => {
 			document.removeEventListener('mousedown', handleClickOutside);
 		};
-	}, []);
-
-	useEffect(() => {
-		setTimeout(() => {
-			const fetchedProducts = [
-				{
-					_id: 1,
-					name: 'Product 1',
-					price: 100,
-					image: [
-						{
-							url: 'https://picsum.photos/500/500?random=1',
-						},
-					],
-				},
-				{
-					_id: 2,
-					name: 'Product 21',
-					price: 100,
-					image: [
-						{
-							url: 'https://picsum.photos/500/500?random=2',
-						},
-					],
-				},
-				{
-					_id: 3,
-					name: 'Product 3',
-					price: 100,
-					image: [
-						{
-							url: 'https://picsum.photos/500/500?random=3',
-						},
-					],
-				},
-				{
-					_id: 4,
-					name: 'Product 4',
-					price: 100,
-					image: [
-						{
-							url: 'https://picsum.photos/500/500?random=4',
-						},
-					],
-				},
-				{
-					_id: 5,
-					name: 'Product 5',
-					price: 100,
-					image: [
-						{
-							url: 'https://picsum.photos/500/500?random=5',
-						},
-					],
-				},
-				{
-					_id: 6,
-					name: 'Product 6',
-					price: 100,
-					image: [
-						{
-							url: 'https://picsum.photos/500/500?random=6',
-						},
-					],
-				},
-				{
-					_id: 7,
-					name: 'Product 7',
-					price: 100,
-					image: [
-						{
-							url: 'https://picsum.photos/500/500?random=7',
-						},
-					],
-				},
-				{
-					_id: 8,
-					name: 'Product 8',
-					price: 100,
-					image: [
-						{
-							url: 'https://picsum.photos/500/500?random=8',
-						},
-					],
-				},
-			];
-			setProducts(fetchedProducts);
-		}, 1000);
 	}, []);
 
 	return (
@@ -142,7 +66,7 @@ const CollectionPage = () => {
 				<SortOptions />
 
 				{/* Product Grid */}
-				<ProductGrid products={products} />
+				<ProductGrid products={products} loading={loading} error={error} />
 			</div>
 		</div>
 	);
